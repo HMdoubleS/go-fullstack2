312e7ddbd8d628fdc532d286d9872dce7c6bb68e
HMdoubleS <hannah.marie.senft@gmail.com>
2022-07-13 12:09:02 -0400

    sending routes into controller folder

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,106 +1,13 @@
 const express = require('express'); // import express
 const router = express.Router(); // can register routes here
 
-const Thing = require('../models/thing'); // importing Thing
+const stuffCtrl = require('../controllers/stuff'); //stuff control constant
 
 // routes
-// sending to the server - POST route - CREATE
-router.post('/', (req, res, next) => {
-    const thing = new Thing({
-      title: req.body.title,
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      price: req.body.price,
-      userId: req.body.userId
-    });
-    thing.save().then(
-      () => {
-        res.status(201).json({
-          message: 'Post saved successfully!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-
-// retrieving from the server - GET route - RECEIVE
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({
-        _id: req.params.id 
-    }).then(
-        (thing) => {
-            res.status(200).json(thing);
-        }
-    ).catch(
-        (error) => {
-            res.status(404).json({
-                error: error
-            });
-        }
-    );
-});
-
-// modify existing data - PUT route - EDIT
-router.put(':id', (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
-      res.status(201).json({
-        message: 'Thing updated successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-// DELETE
-router.delete(':id' , (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-// finding the data from 'Thing' - GET route
-router.get('/', (req, res, next) => {
-    Thing.find().then(
-      (things) => {
-        res.status(200).json(things);
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-
+router.get('/', stuffCtrl.getAllStuff); // finding the data from 'Thing' - GET route
+router.post('/', stuffCtrl.createThing); // sending to the server - POST route - CREATE
+router.get('/:id', stuffCtrl.getOneThing); // retrieving from the server - GET route - RECEIVE
+router.put(':id', stuffCtrl.modifyThing); // modify existing data - PUT route - UPDATE
+router.delete(':id', stuffCtrl.deleteThing); // delete existing data - DELETE
 
 module.exports = router;
