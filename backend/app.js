fa05a8a9bba6fb7332e7b41fbef386047cf862c5
HMdoubleS <hannah.marie.senft@gmail.com>
2022-07-13 16:32:20 -0400

    user authentication

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express'); // imports framework
 const mongoose = require('mongoose'); 
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
 
