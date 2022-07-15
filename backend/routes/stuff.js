6a4c7bd4e6863cb25966abee915f30ca1527816d
HMdoubleS <hannah.marie.senft@gmail.com>
2022-07-14 20:40:58 -0400

    setting up multer and proper item deletion

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -10,7 +10,7 @@ const stuffCtrl = require('../controllers/stuff'); //stuff control constant
 router.get('/', auth, stuffCtrl.getAllStuff); // finding the data from 'Thing' - GET route
 router.post('/', auth, multer, stuffCtrl.createThing); // sending to the server - POST route - CREATE
 router.get('/:id', auth, stuffCtrl.getOneThing); // retrieving from the server - GET route - RECEIVE
-router.put('/:id', auth, stuffCtrl.modifyThing); // modify existing data - PUT route - UPDATE
+router.put('/:id', auth, multer,  stuffCtrl.modifyThing); // modify existing data - PUT route - UPDATE
 router.delete('/:id', auth, stuffCtrl.deleteThing); // delete existing data - DELETE
 
 module.exports = router;
